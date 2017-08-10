1b9cfa24fc6b800a3b2e5fe8770259c3c0a65d83
zjhch123 <416836508@qq.com>
2017-08-10 21:56:55 +0800

    build

--- src/Commons/Header.js
+++ src/Commons/Header.js
@@ -12,9 +12,9 @@ export default class Header extends React.Component {
         <div className={style["g-right"]}>
           <Link to="/" className={style["link-item"]}>首页</Link>
           <NavLink exact to="/result" className={style["link-item"]} activeClassName={style["active"]}>势窗</NavLink>
-          <NavLink exact to="http://139.129.132.196/ics/vis.html" className={style["link-item"]} activeClassName={style["active"]}>势界</NavLink>
+          <a href="http://139.129.132.196/ics/vis.html" className={style["link-item"]}>势界</a>
           <NavLink exact to="/2" className={style["link-item"]} activeClassName={style["active"]}>开放API</NavLink>
-          <NavLink exact to="http://139.129.132.196/ics/help.html" className={style["link-item"]} activeClassName={style["active"]}>帮助</NavLink>
+          <a href="http://139.129.132.196/ics/help.html" className={style["link-item"]}>开放API</a>
           <NavLink exact to="/about" className={style["link-item"]} activeClassName={style["active"]}>关于我们</NavLink>
         </div>
       </header>
--- src/Commons/ResultHeader.js
+++ src/Commons/ResultHeader.js
@@ -10,8 +10,8 @@ export default class ResultHeader extends React.Component {
         <div className={style["g-left"]}>
           <NavLink exact to="/" className={style["link-item"]} activeClassName={style["active"]}>首页</NavLink>
           <NavLink exact to="/result" className={style["link-item"]} activeClassName={style["active"]}>势窗</NavLink>
-          <NavLink exact to="http://139.129.132.196/ics/vis.html" className={style["link-item"]} activeClassName={style["active"]}>势界</NavLink>
-          <NavLink exact to="http://139.129.132.196/ics/help.html" className={style["link-item"]} activeClassName={style["active"]}>开放API</NavLink>
+          <a href="http://139.129.132.196/ics/vis.html" className={style["link-item"]}>势界</a>
+          <a href="http://139.129.132.196/ics/help.html" className={style["link-item"]}>开放API</a>
           <NavLink exact to="/help" className={style["link-item"]} activeClassName={style["active"]}>帮助</NavLink>
           <NavLink exact to="/about" className={style["link-item"]} activeClassName={style["active"]}>关于我们</NavLink>
         </div>
