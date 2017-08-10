5f7012fcc6998b0f58a07dd7a768a9aa5db5a9c8
zjhch123 <416836508@qq.com>
2017-08-10 22:27:26 +0800

    remove old codes

--- _src/Commons/Split.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './Split.scss';
-
-export default function Split(prop) {
-  return (
-    <hr className={style.hr + ' ' + prop.className}/>
-  )
-}
--- _src/Config/Config.js
+++
@@ -1,4 +0,0 @@
-const Config = {
-  url: "http://139.129.132.196:4399/"
-}
-export default Config;
