620cd81d434eb94ea24be678857428e5f4d9a1e8
zjhch123 <416836508@qq.com>
2017-08-19 15:53:54 +0800

    【ADD】增加链接集合

--- src/components/IndexHeader/Header.js
+++ src/components/IndexHeader/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import URL from '../../link';
 import {Link, NavLink} from 'react-router-dom';
 import style from './Header.scss';
 
@@ -7,15 +8,15 @@ export default class Header extends React.Component {
     return (
       <header className={`${style.cHeader} ${this.props.className} f-cb`}>
         <div className={style.gLeft}>
-          <Link to="/" className={`${style.mLogo} ${style.uLinkItem}`}>鉴势</Link>
+          <Link to={URL.Index} className={`${style.mLogo} ${style.uLinkItem}`}>鉴势</Link>
         </div>
         <div className={style.gRight}>
-          <Link to="/" className={style.uLinkItem}>首页</Link>
-          <NavLink exact to="/result" className={style.uLinkItem} activeClassName={style.active}>势窗</NavLink>
-          <a href="http://139.129.132.196/ics/vis.html" className={style.uLinkItem}>势界</a>
-          <NavLink exact to="/open" className={style.uLinkItem} activeClassName={style.active}>开放API</NavLink>
-          <a href="http://139.129.132.196/ics/help.html" className={style.uLinkItem}>帮助</a>
-          <NavLink exact to="/about" className={style.uLinkItem} activeClassName={style.active}>关于我们</NavLink>
+          <Link to={URL.Index} className={style.uLinkItem}>首页</Link>
+          <NavLink exact to={URL.ShiChuang} className={style.uLinkItem} activeClassName={style.active}>势窗</NavLink>
+          <a href={URL.ShiJie} className={style.uLinkItem}>势界</a>
+          <NavLink exact to={URL.OpenAPI} className={style.uLinkItem} activeClassName={style.active}>开放API</NavLink>
+          <a href={URL.Help} className={style.uLinkItem}>帮助</a>
+          <NavLink exact to={URL.About} className={style.uLinkItem} activeClassName={style.active}>关于我们</NavLink>
         </div>
       </header>
     )
---
+++ src/link/index.js
@@ -0,0 +1,8 @@
+export default {
+  Index: '/',
+  ShiJie: 'http://139.129.132.196/ics/vis.html',
+  ShiChuang: '/result',
+  OpenAPI: '/open',
+  Help: 'http://139.129.132.196/ics/help.html',
+  About: '/about'
+}
