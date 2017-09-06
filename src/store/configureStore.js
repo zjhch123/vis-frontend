c37439deaf29d5bc409275cac8369e6f27076a2f
zjhch123 <416836508@qq.com>
2017-09-06 19:39:07 +0800

    【mod】区分生产、线上环境

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,19 +1,5 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import {createLogger} from 'redux-logger';
-import reducers from '../reducers';
-import DevTools from '../containers/DevTools';
-import createHistory from 'history/createBrowserHistory'
-import { routerMiddleware } from 'react-router-redux'
-
-export const history = createHistory();
-
-const routerMiddleWare = routerMiddleware(history);
-
-export const configureStore = () => createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk, routerMiddleWare, createLogger()),
-    DevTools.instrument()
-  )
-);
+if (process.env.NODE_ENV === 'prod') {
+  module.exports = require('./configureStore_prod');
+} else {
+  module.exports = require('./configureStore_dev');
+}
